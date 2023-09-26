d1dcaacef1824d0480c1af33ed48a5d7fc2d2fb1
brunnovaz1 <brunnovaz@gmail.com>
2023-09-25 22:45:18 -0300

    codigo inicial close #28 #23 #24 #27 #25

---
+++ routes/NavegacaoLogin.js
@@ -0,0 +1,16 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import RecuperarSenha from '../screens/RecuperarSenha';
+import CadastroUsuario from '../screens/CadastroUsuario';
+
+const Stack = createNativeStackNavigator();
+
+const NavegacaoLogin = () => {
+  return(
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="CadastroUsuario" component={CadastroUsuario} />
+      <Stack.Screen name="RecuperarSenha" component={RecuperarSenha} />
+    </Stack.Navigator>
+  )
+}
+
+export default NavegacaoLogin
---
+++ screens/CadrastroUsuario.js
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+
+
+const CadastroUsuario = () => {
+  return(
+    <Text>Cadastro Novo Usuário</Text>
+  )
+}
+
+export default CadastroUsuario
