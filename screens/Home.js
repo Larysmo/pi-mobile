44698ba7ab67de2807a0513c697f11c9f58f0b6e
Bruno Souto Vaz <bruno.souto@saude.gov>
2023-10-10 09:54:45 -0300

    navegacoes integradas

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { Appbar, TextInput } from 'react-native-paper';
 import {Ionicons} from '@expo/vector-icons';
@@ -53,66 +52,3 @@ const navigation = useNavigation();
 
 
 export default Home
-=======
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { Appbar, TextInput } from 'react-native-paper';
-import { Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import NavegacaoAbaHome from '../Routes/NavegacaoAbaHome';
-import estilos from '../components/estilos';
-import Pet1 from '../assets/caramelo.jpg';
-import Pet2 from '../assets/gatopreto.png';
-
-const Home = ({ route }) => {
-  const navigation = useNavigation();
-  const handleSair = () => {
-    navigation.navigate('Login');
-  };
-  const { email } = route.params;
-
-const handleLista = () => {
-    navigation.navigate('FlatList');
-  };
-
-
-
-
-
-  return (
-    <View style={{ flex: 1 }}>
-      <Appbar.Header>
-        <Appbar.Content title={`Olá, ${email}`} />
-        <Appbar.Action
-          icon={() => <Ionicons name="exit-outline" size={24} color="black" />}
-          onPress={handleSair}
-        />
-      </Appbar.Header>
-      <View style={estilos.container}>
-        <View style={estilos.quadrado}>
-          <View style={estilos.buscaHome}>
-            <Text style={estilos.tituloHome}> Buscar Cuidador</Text>
-          </View>
-          <View style={estilos.homeBusca}>
-            <TextInput style={estilos.homeInput} label="Cidade Satélite" />
-            <TouchableOpacity style={estilos.homeBotao} onPress={handleLista}>
-              <Text style={estilos.botaoTexto}>Ir</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={estilos.petLista}>
-            <Text style={estilos.titulo}> Seus Pets:</Text>
-            <View style={estilos.pet}>
-              <Image source={Pet1} />
-            </View>
-            <View style={estilos.pet}>
-              <Image source={Pet2} />
-            </View>
-          </View>
-        </View>
-      </View>
-    </View>
-  );
-};
-
-export default Home;
->>>>>>> ff94ad68f5ffe558da1554915f6c50e994a88349
